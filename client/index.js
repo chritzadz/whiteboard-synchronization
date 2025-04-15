6a45b5114990995e1b653f99d5db0de19d8d8df9
Christian Anthony Dumanauw <chrisdumanauw@gmail.com>
2025-04-15 18:47:23 -0300

    enable canvas drawing

--- client/index.js
+++ client/index.js
@@ -1,22 +1,40 @@
+function resizeCanvas(){
+    canvas.width = window.innerWidth - 10;
+    canvas.height = window.innerHeight - 10;
+}
 
-    function resizeCanvas(){
-        canvas.width = window.innerWidth - 10;
-        canvas.height = window.innerHeight - 10;
+const socket = new WebSocket('ws://localhost:8080');
+const canvas = document.getElementById('whiteboard');
+const context = canvas.getContext('2d');
+let id = null;
+let color = null;
+resizeCanvas();
+
+window.addEventListener('resize', resizeCanvas);
+
+let allowDraw = false;
+canvas.addEventListener('mousedown', (e) => {
+    allowDraw = true;
+    context.beginPath();
+    context.moveTo(e.clientX, e.clientY)
+});
+
+canvas.addEventListener('mouseup', (e) => {
+    allowDraw = false;
+});
+
+canvas.addEventListener('mousemove', (e) => {
+    if (!allowDraw) return; //spinlock
+    context.lineTo(e.clientX, e.clientY);
+    context.stroke();
+});
+
+socket.onmessage = (event) => {
+    const data = JSON.parse(event.data);
+
+    if(data.type === 'assign_id') {
+        id = data.id;
     }
-    
-    const socket = new WebSocket('ws://localhost:8080');
-    const canvas = document.getElementById('whiteboard');
-    let id = null;
-    resizeCanvas();
-    
-    window.addEventListener('resize', resizeCanvas);
-
-    socket.onmessage = (event) => {
-        const data = JSON.parse(event.data);
-
-        if(data.type === 'assign_id') {
-            id = data.id;
-        }
-    }
+}
 
 

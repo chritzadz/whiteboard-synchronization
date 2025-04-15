7a0e06168ec510aed22ba6150fc238e8b63444ca
Christian Anthony Dumanauw <chrisdumanauw@gmail.com>
2025-04-15 18:31:13 -0300

    set up client server architecture to manage current socket connection

--- client/index.js
+++ client/index.js
@@ -1,4 +1,4 @@
-window.onload = () => {
+
     function resizeCanvas(){
         canvas.width = window.innerWidth - 10;
         canvas.height = window.innerHeight - 10;
@@ -6,9 +6,17 @@ window.onload = () => {
     
     const socket = new WebSocket('ws://localhost:8080');
     const canvas = document.getElementById('whiteboard');
+    let id = null;
     resizeCanvas();
     
     window.addEventListener('resize', resizeCanvas);
-}
+
+    socket.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+
+        if(data.type === 'assign_id') {
+            id = data.id;
+        }
+    }
 
 
--- server/server.js
+++ server/server.js
@@ -1,7 +1,32 @@
 const WebSocket = require('ws');
 
-const ws = new WebSocket.Server({port:8080});
+const wss = new WebSocket.Server({port:8080});
 
-ws.on('connection', (socket) => {
-    console.log("Connected!");
+let connectedClients = [];
+let clientIdCounter = 1;
+
+wss.on('connection', (socket) => {
+    socket.id = `user-${clientIdCounter++}`;
+    connectedClients.push(socket.id);
+
+    //logs
+    console.log(connectedClients);
+
+    socket.send(JSON.stringify({
+        type: 'client_id',
+        id: socket.id
+    }));
+    console.log(`new connection: ${socket.id}\n`);
+
+
+    socket.on('close', () => {
+        let index = connectedClients.indexOf(socket.id);
+        if(index > -1){
+            connectedClients.splice(index, 1);
+        }
+
+        //logs
+        console.log(connectedClients);
+        console.log(`remove connection: ${socket.id}\n`);
+    });
 });
